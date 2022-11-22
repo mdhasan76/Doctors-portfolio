451cbe8194fef859bbfc8b4ea1e15aa199541fde
Md Hasan <mdhasan8064@gmail.com>
2022-11-22 13:48:20 +0600

    fix payment bug

--- src/pages/dashboard/payment/Checkout.js
+++ src/pages/dashboard/payment/Checkout.js
@@ -1,14 +1,32 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Checkout = ({ bookingData }) => {
     const [cardErr, setCardErr] = useState('')
+    const [clientSecret, setClientSecret] = useState("");
     const stripe = useStripe();
     const elements = useElements();
 
-
     //distructure data 
     const { price, email, pataintName, treatment, _id } = bookingData;
+
+
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("http://localhost:5000/create-payment-intent", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: `bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify({ price }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data.clientSecret)
+                setClientSecret(data.clientSecret)
+            });
+    }, [price]);
 
     //card data submit 
     const handleSubmit = async (event) => {
@@ -33,6 +51,26 @@ const Checkout = ({ bookingData }) => {
         } else {
             setCardErr('')
         }
+
+        const { paymentIntent, error: confirmError } = await
+            stripe.confirmCardPayment(
+                clientSecret,
+                {
+                    payment_method: {
+                        card: card,
+                        billing_details: {
+                            name: pataintName,
+                            email: email
+                        },
+                    },
+                },
+            );
+
+        if (confirmError) {
+            setCardErr(confirmError.message)
+            return;
+        }
+        console.log("error focuse last line", paymentIntent)
     }
     return (
         <div>
@@ -54,7 +92,7 @@ const Checkout = ({ bookingData }) => {
                     }}
                 />
                 <p className='text-red-500 mt-5'>{cardErr}</p>
-                <button type="submit" className='btn btn-sm btn-primary mt-3 text-white' disabled={!stripe}>
+                <button type="submit" className='btn btn-sm btn-primary mt-3 text-white' disabled={!stripe || !clientSecret}>
                     Pay
                 </button>
             </form>
